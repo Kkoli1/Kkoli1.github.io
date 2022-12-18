ba31ae778fd0ab33d8b935d0459ca5943d42ee63
unknown <kkolikkolikkoli@gmail.com>
2022-12-18 18:16:21 +0800

    responsive hero section

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger-menu');
+const links = document.querySelector('.links-wrapper');
+
+hamburger.addEventListener('click', () => {
+    links.classList.toggle('active');
+    hamburger.classList.toggle('active');
+})
