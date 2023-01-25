904fe4fd3ad6ec968c3e0b9693cf6d49166d36be
Kkoli1 <kkolikkolikkoli@gmail.com>
2023-01-25 20:23:02 +0800

    added navigation

--- script.js
+++ script.js
@@ -6,9 +6,48 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
 })
 
+const upArrow = document.querySelector('.up-arrow');
+const downArrow = document.querySelector('.down-arrow');
+const slide = document.querySelector('.slider');
+const carousel = document.querySelector('.carousel');
 
-var w = screen.width;
-console.log(w);
+var direction = -1;
 
-const slider = document.querySelector(".slider");
-console.log(slider.clientHeight);
+upArrow.addEventListener('click', () => {
+    if (direction === -1) {
+        slide.appendChild(slide.firstElementChild);
+        direction = 1;
+    }
+    
+    carousel.style.alignItems = 'flex-end';
+    slide.style.transform = 'translateY(33.316%)';
+
+})
+
+downArrow.addEventListener('click', () => {
+    if(direction === 1){
+        direction = -1;
+    
+        slide.prepend(slide.lastElementChild);
+    }
+    
+    carousel.style.alignItems = 'flex-start';
+    slide.style.transform = 'translateY(-33.316%)';
+})
+
+slide.addEventListener('transitionend', () => {
+    if (direction === -1) {
+        slide.appendChild(slide.firstElementChild);
+        
+    }else if (direction === 1) {
+        slide.prepend(slide.lastElementChild);
+        
+    }
+    
+    slide.style.transition = 'none';
+    slide.style.transform = 'translateY(0%)';
+    setTimeout(() => {
+        slide.style.transition = 'all 0.5s ease-in-out 0s';
+    })
+    
+})
