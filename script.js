b0d0d1f5533a2d9ca7eb20781332e0b5829b6fcb
unknown <kkolikkolikkoli@gmail.com>
2023-02-12 02:04:50 +0800

    issues fixed

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const hamburger = document.querySelector('.hamburger-menu');
 const links = document.querySelector('.links-wrapper');
-
+const body = document.querySelector('body');
 hamburger.addEventListener('click', () => {
     links.classList.toggle('active');
     hamburger.classList.toggle('active');
+    body.classList.toggle('active');
 })
 
 const prevArrow = document.querySelector('.prev-arrow');
