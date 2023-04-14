e7674e2e44ca5431a359febb4ca3f34f33bbf12b
Kkoli1 <kkolikkolikkoli@gmail.com>
2023-04-14 23:31:53 +0800

    added animations

--- scripts/script.js
+++ scripts/script.js
@@ -106,3 +106,18 @@ slide.addEventListener('transitionend', () => {
 })
 
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenLeftElements = document.querySelectorAll('.hidden-left');
+const hiddenRightElements = document.querySelectorAll('.hidden-right');
+hiddenLeftElements.forEach((el) => observer.observe(el));
+hiddenRightElements.forEach((el) => observer.observe(el));
